5cd880d1afa9235c6588649c94cd577df686bfd0
PayneIndustries <33240309+PayneIndustries@users.noreply.github.com>
2017-10-31 15:56:30 -0400

    Main Menu Loop

---
+++ Assets/Scripts/HowToSceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class HowToSceneManager : MonoBehaviour
+{
+    public void OnMouseClick() { Application.LoadLevel("HowToPlayScreen"); }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*        
+//        Developer Name: Alex Dangler
+//         Contribution: Started scene manager script, added functionality to main menu buttond
+//                Feature : Changing of scenes when button is pressed on main menu, allows access to play screen, how to play scene, and quitting the game. 
+//                Start & End dates : 10/31/17 - 10/31/17
+//                References:
+//                        Links:
+//*/
---
+++ Assets/Scripts/QuitSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitSceneManager : MonoBehaviour
+{
+
+    public void OnMouseClick()
+    {
+
+
+
+        Application.Quit();
+    }
+
+}
+
+
+
+    /*        
+//        Developer Name: Alex Dangler
+//         Contribution: Started scene manager script, added functionality to main menu buttond
+//                Feature : Changing of scenes when button is pressed on main menu, allows access to play screen, how to play scene, and quitting the game. 
+//                Start & End dates : 10/31/17 - 10/31/17
+//                References:
+//                        Links:
+//*/
